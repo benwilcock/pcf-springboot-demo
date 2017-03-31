587df235121189074a95845508b04844f1c5bf5b
benwilcock <benwilcock@gmail.com>
2017-03-31 12:12:13 +0100

    build tweaks

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd pcf-springboot-demo-source
+pushd pcf-springboot-demo
   echo "Packaging JAR"
-  ./gradlew clean assemble
+  ./gradlew assemble
 popd
 
-jar_count=`find pcf-springboot-demo-source/target -type f -name *.jar | wc -l`
+jar_count=`find pcf-springboot-demo/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find pcf-springboot-demo-source/target -type f -name *.jar -exec cp "{}" package-output/pcf-springboot-demo.jar \;
+find pcf-springboot-demo/target -type f -name *.jar -exec cp "{}" package-output/pcf-springboot-demo.jar \;
 
 echo "Done packaging"
 exit 0
