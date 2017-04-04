e9807e599cd9e1996140b2b7c0d7d8b9595a5b20
benwilcock <benwilcock@gmail.com>
2017-04-04 18:52:19 +0100

    added an alternative 'disconnected' version of the app in CI pipeline

---
+++ smoke-tests-disconnected/shpec/url_test_shpec.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+describe "pcf-springboot-demo test"
+  describe "HTTP code and content"
+    http_result=`curl -s -o /dev/null -H 'Accept: application/json' $URL -w "%{http_code},%{content_type}"`
+    http_code=`echo $http_result | cut -d, -f1`
+    content_type=`echo $http_result | cut -d, -f2`
+
+    it "serves a 200 response"
+      assert equal $http_code 200
+    end
+
+    it "serves up json"
+      assert match $content_type "application/json"
+    end
+  end
+
+  describe "has messaging and database"
+    http_output=`curl -s -H 'Accept: application/json' $URL`
+
+    it "outputs something"
+        assert present $http_output
+    end
+    it "messaging not present"
+      assert not_match $http_output 'hasmessaging'
+      assert not_match $http_output 'messaging'
+    end
+
+    it "database not present"
+      assert not_match $http_output 'hasdatabase'
+      assert not_match $http_output 'database'
+    end
+
+  end
+end
