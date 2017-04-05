0371e94dacdda8fefd45f20441a5bd2ba8eb7542
benwilcock <benwilcock@gmail.com>
2017-04-05 14:26:23 +0100

    added autowired datasource

--- src/main/java/io/pivotal/PcfSpringBootApplication.java
+++ src/main/java/io/pivotal/PcfSpringBootApplication.java
@@ -2,14 +2,20 @@ package io.pivotal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class PcfSpringBootApplication {
+
+    @Autowired(required = false)
+    DataSource dataSource;
 
     private static final Logger LOG = LoggerFactory.getLogger(PcfSpringBootApplication.class);
 
