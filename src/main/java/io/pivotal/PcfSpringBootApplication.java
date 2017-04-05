6bf92f536990bd4c9a9022cdac2fa99214da7503
benwilcock <benwilcock@gmail.com>
2017-04-05 19:51:40 +0100

    added the active and default profilesto the main page - thanks marcial

--- src/main/java/io/pivotal/PcfSpringBootApplication.java
+++ src/main/java/io/pivotal/PcfSpringBootApplication.java
@@ -2,18 +2,10 @@ package io.pivotal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 
 @SpringBootApplication
@@ -22,15 +14,13 @@ public class PcfSpringBootApplication {
     private static final Logger LOG = LoggerFactory.getLogger(PcfSpringBootApplication.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(PcfSpringBootApplication.class, args);
         LOG.info("Starting the PCF SpringBoot Demo with Thymeleaf.");
     }
 
     @Controller
     class dashboardController {
-
-        @Autowired
-        private Environment env;
 
         @GetMapping("/")
         public String dashboard() {
@@ -110,32 +100,32 @@ public class PcfSpringBootApplication {
 //    }
 //
 
-    @Configuration
-    class CustomInfoEndpoint {
-
-        @Autowired
-        DataSource dataSource;
-
-
-        private boolean hasDatabase() {
-            return !(null == dataSource);
-        }
-
-        @Autowired
-        public void setInfoProperties(ConfigurableEnvironment env) {
-
-            // Add the status to the /info endpoint using Properties
-            Properties props = new Properties();
-
-            // Add the basic status of the resources
-            if(null != dataSource) {
-                props.put("info.hasdatabase", true);
-            }
-
-            if(null != props) {
-                // Make these properties will show up in Spring Boot Actuator's '/info' endpoint...
-                env.getPropertySources().addFirst(new PropertiesPropertySource("extra-info-props", props));
-            }
-        }
-    }
+//    @Configuration
+//    class CustomInfoEndpoint {
+//
+//        @Autowired
+//        DataSource dataSource;
+//
+//
+//        private boolean hasDatabase() {
+//            return !(null == dataSource);
+//        }
+//
+//        @Autowired
+//        public void setInfoProperties(ConfigurableEnvironment env) {
+//
+//            // Add the status to the /info endpoint using Properties
+//            Properties props = new Properties();
+//
+//            // Add the basic status of the resources
+//            if(null != dataSource) {
+//                props.put("info.hasdatabase", true);
+//            }
+//
+//            if(null != props) {
+//                // Make these properties will show up in Spring Boot Actuator's '/info' endpoint...
+//                env.getPropertySources().addFirst(new PropertiesPropertySource("extra-info-props", props));
+//            }
+//        }
+//    }
 }
