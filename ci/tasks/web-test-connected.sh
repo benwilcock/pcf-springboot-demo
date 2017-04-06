aeeefb40894dea92899c787a889f5914b3476869
benwilcock <benwilcock@gmail.com>
2017-04-06 16:45:09 +0100

    adding tests to the build

---
+++ ci/tasks/web-test-connected.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y curl --allow-unauthenticated
+
+set -ex
+
+if [ -z $URL ]; then
+  echo "The URL to test has not been set."
+  exit 1
+fi
+
+# Make sure there the homepage shows there is NO Data Service bound...
+
+if curl -s "$URL" | grep "Ident: mysql"
+then
+    echo "The website [$URL] shows 'Ident: mysql' (as expected)."
+else
+    echo "Error. Not showing 'Ident: mysql' on [$URL]"
+    exit 1
+fi
+
+# Make sure there the homepage shows there is NO Messaging Service bound...
+
+if curl -s "$URL" | grep "Ident: rabbit"
+then
+    echo "The website [$URL] shows 'Ident: rabbit' (as expected)."
+else
+    echo "Error. Not showing 'Ident: rabbit' on [$URL]"
+    exit 1
+fi
+
+# Make sure there the homepage shows there is a Cloud Platform present...
+
+if curl -s "$URL" | grep "API: https://api.run.pivotal.io"
+then
+    echo "The website [$URL] shows 'API: https://api.run.pivotal.io' (as expected)."
+else
+    echo "Error. Not showing 'API: https://api.run.pivotal.io' on [$URL]"
+    exit 1
+fi
+
+exit 0
