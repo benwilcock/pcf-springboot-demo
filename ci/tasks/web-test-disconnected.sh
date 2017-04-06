72bb3acd9eaefa152cd48c5580b2f3da45d7cc1a
benwilcock <benwilcock@gmail.com>
2017-04-06 16:32:47 +0100

    adding tests to the build

--- ci/tasks/web-test-disconnected.sh
+++ ci/tasks/web-test-disconnected.sh
@@ -5,16 +5,38 @@ apt-get update && apt-get install -y curl --allow-unauthenticated
 set -ex
 
 if [ -z $URL ]; then
-  echo "URL not set"
+  echo "The URL to test has not been set."
   exit 1
 fi
 
+# Make sure there the homepage shows there is NO Data Service bound...
+
 if curl -s "$URL" | grep "No Data Service"
 then
-    # if the keyword is in the conent
-    echo "The website [$URL] is working fine"
-    exit 0
+    echo "The website [$URL] shows 'No Data Service' (as expected)."
 else
-    echo "Error"
+    echo "Error. Unexpected Data Service bound to [$URL]"
     exit 1
 fi
+
+# Make sure there the homepage shows there is NO Messaging Service bound...
+
+if curl -s "$URL" | grep "No Messaging Service"
+then
+    echo "The website [$URL] shows 'No Messaging Service' (as expected)."
+else
+    echo "Error. Unexpected Messaging Service bound to [$URL]"
+    exit 1
+fi
+
+# Make sure there the homepage shows there is a Cloud Platform present...
+
+if curl -s "$URL" | grep "No Cloud Platform"
+then
+    echo "The website [$URL] shows 'No Cloud Platform' (this was unexpected)."
+    exit 1
+else
+    echo "The website [$URL] does not show 'No Cloud Platform' (this was expected)."
+fi
+
+exit 0
