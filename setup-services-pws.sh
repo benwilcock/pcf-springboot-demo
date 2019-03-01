14ac9730fddafba850f486fbcee8ba53fa62440a
benwilcock <benwilcock@gmail.com>
2019-03-01 13:46:17 +0000

    added some updates to te manifest and scripts

--- setup-services-pws.sh
+++ setup-services-pws.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cf create-service p-config-server standard config -c config-server-setup.json
+cf create-service p-config-server trial config -c config-server-setup.json
 cf create-service cleardb spark mysql
 cf create-service cloudamqp lemur rabbit
-cf create-service p-service-registry standard registry
+cf create-service p-service-registry trial registry
 cf create-service rediscloud 30mb redis
-cf create-service p-circuit-breaker-dashboard standard breaker
+cf create-service p-circuit-breaker-dashboard trial breaker
