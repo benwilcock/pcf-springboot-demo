510777233a2e77230efdbed9fc86557059a1dc09
benwilcock <benwilcock@gmail.com>
2019-03-04 14:20:34 +0000

    updated the build pipeline

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd pcf-springboot-demo-source
+pushd code
   echo "Packaging JAR"
   ./mvnw package
 popd
 
-jar_count=`find pcf-springboot-demo-source/target -type f -name *.jar | wc -l`
+jar_count=`find code/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting :("
   exit 1
 fi
 
-find pcf-springboot-demo-source/target -type f -name *.jar -exec cp "{}" package-output/pcf-springboot-demo.jar \;
+find code/target -type f -name *.jar -exec cp "{}" package-output/pcf-springboot-demo.jar \;
 
 echo "Done packaging"
 exit 0
