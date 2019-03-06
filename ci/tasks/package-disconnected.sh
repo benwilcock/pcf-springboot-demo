8b6a1cd7c6c208794bf409ed216472b7e4478b1b
benwilcock <benwilcock@gmail.com>
2019-03-06 11:04:11 +0000

    split the application packaging steps into two

---
+++ ci/tasks/package-disconnected.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e +x
+
+pushd code
+  echo "Building and packaging the application JAR with Maven..."
+  mvn package -DskipTests=true
+popd
+
+jar_count=`find code/target -type f -name *.jar | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting :("
+  exit 1
+fi
+
+find code/target -type f -name *.jar -exec cp "{}" package-output/pcf-springboot-demo.jar \;
+
+echo "Done packaging"
+exit 0
